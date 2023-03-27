44ed42804dac493fe6ebcfc470732658688ac17b
realovexx <jorgevega.0vx@gmail.com>
2023-03-27 18:36:34 -0300

    agregando hasta ej4 practica arreglos

--- EjArreglos.new/src/ejarreglos/Ej3.java
+++ EjArreglos.new/src/ejarreglos/Ej3.java
@@ -5,6 +5,8 @@
  */
 package ejarreglos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author OvExX
@@ -15,7 +17,49 @@ public class Ej3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+/*Recorrer un vector de N enteros contabilizando cuántos números son de
+1 dígito, cuántos de 2 dígitos, etcétera (hasta 5 dígitos).
+*/
+Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la dimension del vector");
+        int dim = leer.nextInt();
+        int[] vector = new int[dim];
+        System.out.println("se completara el vector de forma aleatoria");
+        for (int i = 0 ; i < dim ; i++){
+            vector[i] = (int)(Math.random()*100000);
+        }
+       /* System.out.println("A continuación, ingrese los "+dim+" numeros:");
+          for (int i = 0 ; i < dim ; i++){
+            vector[i] = leer.nextInt();
+        }*/
+        int undig = 0;
+        int dosdig = 0;
+        int tresdig = 0;
+        int cuadig = 0;
+        int cinqdig = 0;
+        for (int j = 0 ; j < dim ; j++){
+            if (vector[j]<10){
+                undig++;
+            }else if (vector[j]<100 && vector[j]>=10){
+                dosdig++;
+            }else if (vector[j]<1000 && vector[j]>=100){
+                tresdig++;
+            }else if (vector[j]<10000 && vector[j]>=1000){
+                cuadig++;
+            }else if (vector[j]<100000 && vector[j]>=10000){
+                cinqdig++;
+            }
+        }
+        
+        System.out.println("El vector contiene "+undig+" numeros de 1 digito, "+dosdig+" numeros de 2 digitos");
+        System.out.println(tresdig+" numeros de 3 digitos, "+cuadig+" numeros de 4 digitos y "+cinqdig+" de 5.");
+        System.out.println("A continuacion, se muestra el vector para corroborar");
+        System.out.println("");
+        for (int k = 0; k < dim; k++) {
+            System.out.print("["+vector[k]+"]");
+        }
+        System.out.println("");
+        
     }
     
 }
---
+++ EjArreglos.new/src/ejarreglos/Ej4.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejarreglos;
+
+/**
+ *
+ * @author OvExX
+ */
+public class Ej4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+/*Realizar un programa que rellene un matriz de 4 x 4 de valores aleatorios
+y muestre la traspuesta de la matriz. La matriz traspuesta de una matriz
+A se denota por B y se obtiene cambiando sus filas por columnas (o
+viceversa).
+*/
+        System.out.println("Se generara automaticamente una matriz de 4x4");
+        int[][] matriz = new int[4][4];
+        int[][] matrizT = new int [4][4];
+        
+        for (int i = 0 ; i < 4 ; i++){
+            for (int j = 0 ; j < 4 ; j++){
+                matriz[i][j] = (int) (Math.random()*10);
+            }
+        }
+        for (int k = 0 ; k < 4 ; k++){
+            for (int l = 0 ; l < 4 ; l++){
+                matrizT[k][l] = matriz[l][k];
+            }
+        }
+        
+        System.out.println("A continuacion, se mostrara la matriz original, seguida de su traspuesta");
+        System.out.println("");
+        for (int m = 0; m < 4; m++) {
+            for (int n = 0; n < 4; n++) {
+                System.out.print("["+matriz[m][n]+"]");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        for (int o = 0; o < 4; o++) {
+            for (int p = 0; p < 4; p++) {
+                System.out.print("["+matrizT[o][p]+"]");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
