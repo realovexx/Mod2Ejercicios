3d49d4858a4fa646b15ba1eb07f3b2fc67d0a32d
realovexx <jorgevega.0vx@gmail.com>
2023-03-31 18:59:31 -0300

    Agrego inicio de POO

---
+++ POO.Teoria/src/Entidad/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+/**
+ *
+ * @author ovexx
+ */
+public class Persona {
+  public String Nombre;
+  public int Edad;
+  public String Genero;
+
+    public Persona(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+  
+  
+}
